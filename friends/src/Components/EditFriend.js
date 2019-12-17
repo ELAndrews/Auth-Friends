47e6107a55bdf9d3f6e07f65e5ac135b751bd81b
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-12-17 15:36:13 +0000

    Add form functionality to add new friend to list and display

--- friends/src/Components/EditFriend.js
+++ friends/src/Components/EditFriend.js
@@ -1,18 +1,52 @@
-import React from "react";
-import axios from "axios";
+import React, { useState } from "react";
+import axiosWithAuth from "./axiosWithAuth";
 
 export default function EditFriends(props) {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    age: "",
+    email: ""
+  });
+  const [error, setError] = useState(null);
+
+  const handleChange = e => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    axiosWithAuth()
+      .post("http://localhost:5000/api/friends", formValues)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        setError(err);
+      });
+  };
+
   return (
-    <div className="friends">
-      {props.friends.map((curr, index) => {
-        return (
-          <div key={index} className="friendCard">
-            <p>{curr.name}</p>
-            <span>age: {curr.age}</span> <br />
-            <span>email: {curr.email}</span>
-          </div>
-        );
-      })}
+    <div className="editFriendsContainer">
+      <div className="friends">
+        {props.friends.map((curr, index) => {
+          return (
+            <div key={index} className="friendCard">
+              <p>{curr.name}</p>
+              <span>age: {curr.age}</span> <br />
+              <span>email: {curr.email}</span>
+            </div>
+          );
+        })}
+      </div>
+      <div className="form">
+        <h3>Add a New Friend</h3>
+        <form onSubmit={handleSubmit}>
+          Name: <input type="text" name="name" onChange={handleChange} /> <br />
+          Age: <input type="text" name="age" onChange={handleChange} />
+          <br />
+          Email: <input type="email" name="email" onChange={handleChange} />
+          <button>Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
--- friends/src/Components/UserDashboard.js
+++ friends/src/Components/UserDashboard.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axiosWithAuth from "./axiosWithAuth";
 import FriendsList from "./FriendsList";
 import EditFriends from "./EditFriend";
 import { Route, NavLink } from "react-router-dom";
@@ -9,12 +9,8 @@ export default function UserDashboard() {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios
-      .get("http://localhost:5000/api/friends", {
-        headers: {
-          Authorization: localStorage.getItem("token")
-        }
-      })
+    axiosWithAuth()
+      .get("http://localhost:5000/api/friends")
       .then(res => {
         setFriends(res.data);
       })
